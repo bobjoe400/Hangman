77406edabadccabc4dd00ba7def2bc66109b30f4
bobjoe400 <cnamattern@gmail.com>
2016-05-07 12:40:28 -0700

    Added background music. The actual music isnt set but its working.

--- Hangman/src/main/MainWindow.java
+++ Hangman/src/main/MainWindow.java
@@ -1,9 +1,15 @@
 package main;
+
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,65 +17,82 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class MainWindow extends JFrame {
-	
+
 	private int remainingGuesses;
 	private String wrongGuesses;
 	private String word;
 	private String visible;
 
+	public class MusicThread implements Runnable {
+		public void run() {
+				try {
+					FileInputStream fs = new FileInputStream("Resources/music.wav");
+					BufferedInputStream buffered = new BufferedInputStream(fs);
+					AudioInputStream inputStream = AudioSystem.getAudioInputStream(buffered);
+					Clip clip = AudioSystem.getClip();
+					clip.open(inputStream);
+					clip.loop(Clip.LOOP_CONTINUOUSLY);
+					while(true){}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+		}
+	}
+
 	public MainWindow(String toGuess) {
+		Thread music = new Thread(new MusicThread());
+		music.start();
 		remainingGuesses = 10;
 		wrongGuesses = "";
 		word = toGuess;
 
 		visible = "";
 
-		for(int i = 0; i < word.length(); ++i) {
+		for (int i = 0; i < word.length(); ++i) {
 			visible += "_ ";
 		}
 
 		JPanel corePanel = new JPanel();
 		corePanel.setLayout(new BorderLayout());
-		
-		final JLabel status = new JLabel("You have "+remainingGuesses+" remaining", SwingConstants.CENTER);
-		final JLabel wrong = new JLabel("Wrong guesses so far: "+wrongGuesses);
+
+		final JLabel status = new JLabel("You have " + remainingGuesses + " remaining", SwingConstants.CENTER);
+		final JLabel wrong = new JLabel("Wrong guesses so far: " + wrongGuesses);
 		final JLabel visibleLabel = new JLabel(visible, SwingConstants.CENTER);
-		final JTextField input = new JTextField(); 
-		
+		final JTextField input = new JTextField();
+
 		JPanel southPanel = new JPanel(new GridLayout(4, 1));
 		southPanel.add(status);
 		southPanel.add(visibleLabel);
 		southPanel.add(input);
 		southPanel.add(wrong);
-		
+
 		corePanel.add(southPanel, BorderLayout.SOUTH);
-		
+
 		final HangmanFigure hf = new HangmanFigure();
 		corePanel.add(hf, BorderLayout.CENTER);
 
 		this.add(corePanel, BorderLayout.CENTER);
-		
+
 		input.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+
 				String text = input.getText();
-				
-				if(text.length()  == 1 && text.matches("[a-z]")) {
-					
+
+				if (text.length() == 1 && text.matches("[a-z]")) {
+
 					boolean guessFound = false;
-					
-					for(int i = 0; i < word.length(); ++i) {
-						if(text.charAt(0) == word.charAt(i)) {
+
+					for (int i = 0; i < word.length(); ++i) {
+						if (text.charAt(0) == word.charAt(i)) {
 							guessFound = true;
-							
+
 							String newVisible = "";
-							for(int j = 0; j < visible.length(); ++j) {
-								if(j == (i*2)) {
+							for (int j = 0; j < visible.length(); ++j) {
+								if (j == (i * 2)) {
 									newVisible += word.charAt(i);
-								}
-								else {
+								} else {
 									newVisible += visible.charAt(j);
 								}
 							}
@@ -77,48 +100,44 @@ public class MainWindow extends JFrame {
 							visibleLabel.setText(visible);
 						}
 					}
-					
-					if(!guessFound) {
-						if(--remainingGuesses >= 0) {
-							status.setText("You have "+remainingGuesses+" guesses remaining");
-							wrongGuesses += text+" ";
-							wrong.setText("Wrong guesses so far: "+wrongGuesses);
+
+					if (!guessFound) {
+						if (--remainingGuesses >= 0) {
+							status.setText("You have " + remainingGuesses + " guesses remaining");
+							wrongGuesses += text + " ";
+							wrong.setText("Wrong guesses so far: " + wrongGuesses);
 							hf.set();
-						}
-						else {
-							status.setText("You lost: the word was "+word);
+						} else {
+							status.setText("You lost: the word was " + word);
 							input.setEnabled(false);
 						}
-					}
-					else {
+					} else {
 						String actualVisible = "";
-						for(int i = 0; i < visible.length(); i+=2) {
+						for (int i = 0; i < visible.length(); i += 2) {
 							actualVisible += visible.charAt(i);
 						}
-						
-						if(actualVisible.equals(word)) {
+
+						if (actualVisible.equals(word)) {
 							status.setText("Congratulations, you have won!");
 							input.setEnabled(false);
 						}
 					}
-					
-				}
-				else {
+
+				} else {
 					System.out.println("Invalid input!");
 				}
-				
+
 				input.setText("");
 			}
-			
+
 		});
-		
+
 		this.pack();
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
-	
-	
+
 	public static void main(String[] args) {
 		new MainWindow("cat");
 	}
