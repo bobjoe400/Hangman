f53a0976ea2a09eae9c7023d38a885c203679c6c
bobjoe400 <cnamattern@gmail.com>
2016-05-07 13:31:10 -0700

    Start on basic menu system

--- Hangman/src/main/MainWindow.java
+++ Hangman/src/main/MainWindow.java
@@ -6,42 +6,47 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
+import java.util.ArrayList;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
-public class MainWindow extends JFrame {
+public class MainWindow extends JFrame{
 
 	private int remainingGuesses;
+	private static int diff;
 	private String wrongGuesses;
 	private String word;
 	private String visible;
+	
 
-	public class MusicThread implements Runnable {
+	static class MusicThread implements Runnable {
 		public void run() {
-				try {
-					FileInputStream fs = new FileInputStream("Resources/music.wav");
-					BufferedInputStream buffered = new BufferedInputStream(fs);
-					AudioInputStream inputStream = AudioSystem.getAudioInputStream(buffered);
-					Clip clip = AudioSystem.getClip();
-					clip.open(inputStream);
-					clip.loop(Clip.LOOP_CONTINUOUSLY);
-					while(true){}
-				} catch (Exception e) {
-					e.printStackTrace();
+			try {
+				FileInputStream fs = new FileInputStream("Resources/music.wav");
+				BufferedInputStream buffered = new BufferedInputStream(fs);
+				AudioInputStream inputStream = AudioSystem.getAudioInputStream(buffered);
+				Clip clip = AudioSystem.getClip();
+				clip.open(inputStream);
+				clip.loop(Clip.LOOP_CONTINUOUSLY);
+				while (true) {
 				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
+	public MainWindow(String toGuess, int difficulty) {
 
-	public MainWindow(String toGuess) {
-		Thread music = new Thread(new MusicThread());
-		music.start();
 		remainingGuesses = 10;
 		wrongGuesses = "";
 		word = toGuess;
@@ -59,7 +64,9 @@ public class MainWindow extends JFrame {
 		final JLabel wrong = new JLabel("Wrong guesses so far: " + wrongGuesses);
 		final JLabel visibleLabel = new JLabel(visible, SwingConstants.CENTER);
 		final JTextField input = new JTextField();
+		
 
+		
 		JPanel southPanel = new JPanel(new GridLayout(4, 1));
 		southPanel.add(status);
 		southPanel.add(visibleLabel);
@@ -77,7 +84,6 @@ public class MainWindow extends JFrame {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
 				String text = input.getText();
 
 				if (text.length() == 1 && text.matches("[a-z]")) {
@@ -135,10 +141,59 @@ public class MainWindow extends JFrame {
 		this.pack();
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		while(diff == -1){
+			System.out.println(diff);
+		}
 		this.setVisible(true);
 	}
 
 	public static void main(String[] args) {
-		new MainWindow("cat");
+		Thread music = new Thread(new MusicThread());
+		music.start();
+		diff = -1;
+		new Menu();
+		new MainWindow("cat",diff);
+	}
+	static class Menu extends JFrame implements ActionListener{
+		
+		private ArrayList<JMenuItem> diffs;
+		
+		public Menu(){
+			diffs = new ArrayList<JMenuItem>();
+			JMenuBar menuBar = new JMenuBar();
+			JMenu menu1 = new JMenu("Difficulty");
+			diffs.add(new JMenuItem("Easy"));
+			diffs.add(new JMenuItem("Medium"));
+			diffs.add(new JMenuItem("Hard"));
+			for (JMenuItem item : diffs) {
+				item.addActionListener(this);
+				menu1.add(item);
+			}
+			menuBar.add(menu1);
+			this.setJMenuBar(menuBar);
+			this.setTitle("Main menu");
+			this.setSize(300, 300);
+			this.setVisible(true);
+		}
+		
+		public void actionPerformed(ActionEvent E){
+			if(E.getSource() == diffs.get(0)){
+				System.out.println("Easy");
+				diff = 0;
+			}
+			if(E.getSource() == diffs.get(1)){
+				System.out.println("Med");
+				diff = 1;
+			}
+			if(E.getSource() == diffs.get(2)){
+				System.out.println("Hard");
+				diff = 2;
+			}
+			checkVisible();
+		}
+		private void checkVisible(){
+			this.setVisible((diff == -1)? true: false);
+		}
+
 	}
 }
