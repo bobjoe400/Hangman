a44d8c9eb6cdfeae31719a51410027f4b63427c2
bobjoe400 <cnamattern@gmail.com>
2016-05-09 19:00:39 -0700

    fixed the board not drawing in the right place.

--- Hangman/src/main/Instructions.java
+++ Hangman/src/main/Instructions.java
@@ -5,12 +5,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 
-public class Instructions extends JFrame implements ActionListener {
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
+public class Instructions extends JFrame {
+	
 
 }
